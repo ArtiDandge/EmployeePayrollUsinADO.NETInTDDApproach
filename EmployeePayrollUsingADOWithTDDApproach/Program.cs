0494ba06e648092d6372db261c05d5d8ac911a5d
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-30 16:26:54 +0530

    [Arti Dandge] Add : Display Welcome Message on console

---
+++ EmployeePayrollUsingADOWithTDDApproach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollUsingADOWithTDDApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Project with ADO solved using TDD Approach");
+        }
+    }
+}
