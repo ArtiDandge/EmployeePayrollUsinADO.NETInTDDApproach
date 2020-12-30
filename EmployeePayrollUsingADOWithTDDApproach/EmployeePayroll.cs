a67dd958137d1d1dd15224e2a3bb08f08b1767fe
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-30 16:48:00 +0530

    [Arti Dandge] Add : Ability to establish database connection

---
+++ EmployeePayrollUsingADOWithTDDApproach/EmployeePayroll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollUsingADOWithTDDApproach
+{
+    public class EmployeePayroll
+    {
+        private static SqlConnection Connectionsetup()
+        {
+            return new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=employee_payroll_service;Integrated Security=True");
+        }
+    }
+}
